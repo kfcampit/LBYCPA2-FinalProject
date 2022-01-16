3f5797235c5761d1b56ad862be7117ec7a72db18
kobiRasing <kobirasing7@gmail.com>
2022-01-16 17:48:04 +0800

    CreateController class

---
+++ src/main/java/com/dlsu/lbycpa2finalproject/CreateController.java
@@ -0,0 +1,45 @@
+package com.dlsu.lbycpa2finalproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreateController {
+
+    @FXML
+    private TextField choice1;
+
+    @FXML
+    private TextArea inputQuestion;
+
+    @FXML
+    private TextField choice2;
+
+    @FXML
+    private TextField choice3;
+
+    @FXML
+    private TextField choice4;
+
+    @FXML
+    private TextField correctAnswer;
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    void onClickBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSubmit(ActionEvent event) {
+
+    }
+
+}
