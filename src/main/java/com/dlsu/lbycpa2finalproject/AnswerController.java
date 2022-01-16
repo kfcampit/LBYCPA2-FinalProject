bf38db0dcb9d731a85726a24d16f9591b43a988a
kobiRasing <kobirasing7@gmail.com>
2022-01-16 15:53:39 +0800

    AnswerController updated

--- src/main/java/com/dlsu/lbycpa2finalproject/AnswerController.java
+++ src/main/java/com/dlsu/lbycpa2finalproject/AnswerController.java
@@ -1,4 +1,45 @@
 package com.dlsu.lbycpa2finalproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class AnswerController {
+
+    @FXML
+    private Button redOption;
+
+    @FXML
+    private Button blueOption;
+
+    @FXML
+    private Button yellowOption;
+
+    @FXML
+    private Button greenOption;
+
+    @FXML
+    private Label userQuestion; /* dito lalabas yung question ng user */
+
+    @FXML
+    void onClickBlueOption(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickGreenOption(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickRedOption(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickYellowOption(ActionEvent event) {
+
+    }
+
 }
