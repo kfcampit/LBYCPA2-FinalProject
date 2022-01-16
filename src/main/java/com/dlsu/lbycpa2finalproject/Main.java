65e81c3a4f0fba8b3fe06ce9be3c190b13a0bf8b
kobiRasing <kobirasing7@gmail.com>
2022-01-16 19:41:33 +0800

    All Controller Classes

--- src/main/java/com/dlsu/lbycpa2finalproject/Main.java
+++ src/main/java/com/dlsu/lbycpa2finalproject/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
     private static Scene s;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Leaderboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
         s = new Scene(fxmlLoader.load(), 643, 689);
         stage.setTitle("Welcome to Quiztify!");
         stage.setScene(s);
