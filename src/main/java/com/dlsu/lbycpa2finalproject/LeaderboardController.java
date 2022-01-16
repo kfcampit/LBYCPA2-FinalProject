77773b1b3d329e7be593049c230f6782abc1885f
kobiRasing <kobirasing7@gmail.com>
2022-01-16 19:37:27 +0800

    All Controller Classes

---
+++ src/main/java/com/dlsu/lbycpa2finalproject/LeaderboardController.java
@@ -0,0 +1,48 @@
+package com.dlsu.lbycpa2finalproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LeaderboardController {
+
+    @FXML
+    private Label name1;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Label score1;
+
+    @FXML
+    private Label name2;
+
+    @FXML
+    private Label score2;
+
+    @FXML
+    private Label name3;
+
+    @FXML
+    private Label score3;
+
+    @FXML
+    private Label name4;
+
+    @FXML
+    private Label score4;
+
+    @FXML
+    private Label name5;
+
+    @FXML
+    private Label score5;
+
+    @FXML
+    void onClickBack(ActionEvent event) {
+        System.out.println("Back");
+    }
+
+}
