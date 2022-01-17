7611426a6844466e6469001bd597eacf6aa11091
kfcampit <75428470+kfcampit@users.noreply.github.com>
2022-01-17 23:57:15 +0800

    Added Login Controller

--- src/main/java/com/dlsu/lbycpa2finalproject/LoginController.java
+++ src/main/java/com/dlsu/lbycpa2finalproject/LoginController.java
@@ -1,5 +1,6 @@
 package com.dlsu.lbycpa2finalproject;
 
+import com.dlsu.lbycpa2finalproject.backend.AccountController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,19 +9,65 @@ import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+
 public class LoginController {
+    private AccountController ac = new AccountController();
+
     @FXML
     public TextField username, password_text;
     @FXML
     public CheckBox showPassword;
     @FXML
     public PasswordField password_hidden;
+    @FXML
+    public Text errorText;
 
     @FXML
-    public void onLoginButtonClick() {
+    public void onLoginButtonClick() throws ExecutionException, InterruptedException {
+        String pass = "";
+        if (Objects.equals(username.getText(), "") || (Objects.equals(password_hidden.getText(), "") && Objects.equals(password_text.getText(), ""))) {
+            errorText.setText("Please complete all the details");
+            errorText.setVisible(true);
+            return;
+        } else if (Objects.equals(password_hidden.getText(), "")) {
+            pass = password_text.getText();
+        } else if (Objects.equals(password_text.getText(), "")) {
+            pass = password_hidden.getText();
+        }
+        if (ac.checkDetails(username.getText(), pass, true) == 1) {
+            System.out.println("Login Successful!");
+        } else {
+            errorText.setText("Wrong username or password");
+            errorText.setVisible(true);
+            password_text.clear();
+        }
+    }
 
+    @FXML
+    public void onRegisterButtonClick() throws ExecutionException, InterruptedException {
+        String pass = "";
+        if (Objects.equals(username.getText(), "") || (Objects.equals(password_hidden.getText(), "") && Objects.equals(password_text.getText(), ""))) {
+            errorText.setText("Please complete all the details");
+            errorText.setVisible(true);
+            return;
+        } else if (Objects.equals(password_hidden.getText(), "")) {
+            pass = password_text.getText();
+        } else if (Objects.equals(password_text.getText(), "")) {
+            pass = password_hidden.getText();
+        }
+        if (ac.checkDetails(username.getText(), pass, false) == -1) {
+            errorText.setText("Account already exists");
+            errorText.setVisible(true);
+            password_text.clear();
+        } else {
+            ac.registerAccount(username.getText(), password_hidden.getText());
+            System.out.println("Registration Successful!");
+        }
     }
 
     @FXML
@@ -35,4 +82,6 @@ public class LoginController {
         password_hidden.setVisible(true);
         password_text.setVisible(false);
     }
+
+
 }
---
+++ src/main/java/com/dlsu/lbycpa2finalproject/backend/AccountController.java
@@ -0,0 +1,94 @@
+package com.dlsu.lbycpa2finalproject.backend;
+
+import com.dlsu.lbycpa2finalproject.LoginController;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class AccountController {
+    private Firestore db;
+    private int numAccounts;
+    private static String userID;
+
+    public AccountController() {
+        db = FirestoreClient.getFirestore();
+    }
+
+    public int checkDetails(String user, String pass, boolean forLogin) throws ExecutionException, InterruptedException {
+        Hashtable<String, String> hashtable = new Hashtable<>();
+        ApiFuture<QuerySnapshot> query = db.collection("accounts").get();
+        QuerySnapshot querySnapshot = query.get();
+
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document: documents) {
+            hashtable.put(document.getString("username"), document.getString("password"));
+        }
+
+        numAccounts = hashtable.size();
+
+        if (forLogin && hashtable.containsKey(user) && hashtable.containsValue(pass))
+            return 1; //Affirm Login
+        if (hashtable.containsKey(user))
+            return -1; //User already exists
+        return 0; //User does not exist
+    }
+
+    public void registerAccount(String user, String pass) {
+
+        Map<String, Object> account = new HashMap<>();
+        account.put("username", user);
+        account.put("password", pass);
+        account.put("answersCorrect", 0);
+        account.put("answerIncorrect", 0);
+        account.put("quizzesAnswered", 0);
+
+        String accountID = String.format("%04d", numAccounts + 1) + "-" + user.toLowerCase();
+        db.collection("accounts").document(accountID).set(account);
+        setUserID(accountID);
+    }
+
+    public Hashtable<String, Hashtable<String, Object>> getPlayerStatistics() throws ExecutionException, InterruptedException {
+        Hashtable<String, Hashtable<String, Object>> hashtable = new Hashtable<>();
+        Hashtable<String, Object> temp;
+        ApiFuture<QuerySnapshot> query = db.collection("accounts").get();
+        QuerySnapshot querySnapshot = query.get();
+
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document: documents) {
+            temp = new Hashtable<>();
+            temp.put("username", document.getString("username"));
+            temp.put("password", document.getString("password"));
+            temp.put("answersCorrect", Math.toIntExact(document.getLong("answersCorrect")));
+            temp.put("answerIncorrect", Math.toIntExact(document.getLong("answerIncorrect")));
+            temp.put("quizzesAnswered", Math.toIntExact(document.getLong("quizzesAnswered")));
+
+            hashtable.put(document.getId(), temp);
+        }
+        return hashtable;
+    }
+
+    public Hashtable<String, Object> getCurrentUserData(String userID) throws ExecutionException, InterruptedException {
+        return getPlayerStatistics().get(userID);
+    }
+
+    public void updateCurrentUserData(String userID, Hashtable<String, Object> data) {
+        db.collection("accounts").document(userID).set(data);
+    }
+
+    private void setUserID(String userID) {
+        AccountController.userID = userID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+}
