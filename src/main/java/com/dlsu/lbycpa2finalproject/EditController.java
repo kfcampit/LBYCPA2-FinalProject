3ff41816e775b9ebdddba4f11e7b3960345532ee
kobiRasing <kobirasing7@gmail.com>
2022-01-19 17:56:26 +0800

    error edit

--- src/main/java/com/dlsu/lbycpa2finalproject/EditController.java
+++ src/main/java/com/dlsu/lbycpa2finalproject/EditController.java
@@ -1,14 +1,28 @@
 package com.dlsu.lbycpa2finalproject;
 
+import com.dlsu.lbycpa2finalproject.backend.QuizObject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 
-public class EditController extends Main{
+public class EditController extends EditQuizListController implements Initializable {
+    QuizObject qz;
+    int qNumber = 0; /* question number */
+
+    @FXML
+    private Label topic;
 
     @FXML
     private TextField choice1;
@@ -29,7 +43,10 @@ public class EditController extends Main{
     private TextField correctAnswer;
 
     @FXML
-    private Button edit;
+    private Button next;
+
+    @FXML
+    private Button prev;
 
     @FXML
     private Button home;
@@ -40,6 +57,9 @@ public class EditController extends Main{
     @FXML
     private Button search;
 
+    public EditController() throws ExecutionException, InterruptedException {
+    }
+
     @FXML
     void onClickHome(ActionEvent event) throws IOException {
         //System.out.println("back");
@@ -47,13 +67,60 @@ public class EditController extends Main{
     }
 
     @FXML
-    void onClickEdit(ActionEvent event) {
-        System.out.println("edit");
+    void onClickNext(ActionEvent event) {
+        System.out.println("next");
     }
 
     @FXML
-    void onClickSearch(ActionEvent event) {
-        System.out.println("search");
+    void onClickPrev(ActionEvent event) {
+
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            qz = qc.getQuiz(clickedId);
+            topic.setText(qz.getTopic());
+            inputQuestion.setText(qz.getQuestionList().get(0).getQuestion());
+            choice1.setText(qz.getQuestionList().get(0).getChoices()[0]);
+            choice2.setText(qz.getQuestionList().get(0).getChoices()[1]);
+            choice3.setText(qz.getQuestionList().get(0).getChoices()[2]);
+            choice4.setText(qz.getQuestionList().get(0).getChoices()[3]);
+            int ansIndex = qz.getQuestionList().get(0).getAnswer();
+            correctAnswer.setText(qz.getQuestionList().get(0).getChoices()[ansIndex]);
+            try {
+                imageView.setImage(new Image(qz.getQuestionList().get(0).getImageURL()));
+                centerImage();
+            } catch (Exception ignore) {
+
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void centerImage() {
+        Image img = imageView.getImage();
+        if (img != null) {
+            double w = 0;
+            double h = 0;
+
+            double ratioX = imageView.getFitWidth() / img.getWidth();
+            double ratioY = imageView.getFitHeight() / img.getHeight();
+
+            double reducCoeff = 0;
+            if(ratioX >= ratioY) {
+                reducCoeff = ratioY;
+            } else {
+                reducCoeff = ratioX;
+            }
+
+            w = img.getWidth() * reducCoeff;
+            h = img.getHeight() * reducCoeff;
+
+            imageView.setX((imageView.getFitWidth() - w) / 2);
+            imageView.setY((imageView.getFitHeight() - h) / 2);
+
+        }
+    }
 }
